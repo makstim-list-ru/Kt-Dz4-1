4909b9448dd4dcd30367bd13545d3c15bce2e92f
Maksim Timofeyev <makstim@list.ru>
2024-05-18 06:29:24 +0300

    Commit 1 - not working coverage visualisation

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -7,6 +7,47 @@ import org.junit.Assert.*
 class MainKtTest {
 
     @Test
-    fun commissionCalc() {
+    fun commissionCalc_MC150K() {
+        val cardType: String = "MC"
+        val sumMonthTransfers: Int = 0
+        val sumDayTransfers: Int = 0
+        val newTransfer: Int = 150_000
+
+        val result = commissionCalc(
+            cardType = cardType,
+            sumMonthTransfers = sumMonthTransfers,
+            sumDayTransfers = sumDayTransfers,
+            newTransfer = newTransfer
+        )
+        assertEquals(470, result)
+
+    }
+
+    @Test
+    fun commissionCalc_MC700K() {
+        val cardType: String = "MC"
+        val sumMonthTransfers: Int = 0
+        val sumDayTransfers: Int = 0
+        val newTransfer: Int = 700_000
+
+        val result = commissionCalc(
+            cardType = cardType,
+            sumMonthTransfers = sumMonthTransfers,
+            sumDayTransfers = sumDayTransfers,
+            newTransfer = newTransfer
+        )
+        assertEquals(-1, result)
+
+    }
+
+    @Test
+    fun commissionCalc_default150K() {
+        val newTransfer: Int = 150_000
+
+        val result = commissionCalc(
+            newTransfer = newTransfer
+        )
+        assertEquals(1125, result)
+
     }
 }
